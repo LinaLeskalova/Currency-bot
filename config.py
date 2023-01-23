4bac28f5c15c1444c4712a404b5062ba9cdb0ae9
unknown <linavnikk@ya.ru>
2023-01-23 23:53:13 +0300

    A new file is added

---
+++ config.py
@@ -0,0 +1,22 @@
+TOKEN = '5866489906:AAGEBRT-6KumFOkkWu2haWmf8OpWGnBTJwQ'
+APIKEY = '02a906c81e6eeddb6dd52258c62b8e3c6a088b499b8dff228588ce12bada54c0'
+
+keys = {
+    "биткоин": "BTC",
+    "эфириум": "ETH",
+    "юань": "CNY",
+    "доллар США": "USD",
+    "евро": "EUR",
+    "йена": "JPY",
+    "рубль": "RUB",
+    "вона": "KRW",
+    "злотый": "PLN",
+    "форинт": "HUF",
+    "дирхам": "AED",
+    "рупия": "INR",
+    "песо": "MXN",
+    "лей": "RON",
+    "франк": "CHF",
+    "крона": "CZK",
+    "реал": "BRL",
+    }
